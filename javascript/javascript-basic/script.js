9a658cbffc286092e48655d8d38ef2011be91419
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-03-14 10:50:28 -0600

    Add scope explanation with var

--- javascript/javascript-basic/script.js
+++ javascript/javascript-basic/script.js
@@ -1,12 +1,25 @@
 console.log("Hello world");
 
+//declaration + assign
 var myVariable = "Something";
 let mySec = true;
 const PI = 3.14;
 
+//declatration
+let dog;
+
+//assign
+dog = "Lablador";
+
 //comment one line
 
-/* 
+// thats why do not use var
+{
+  var myVariable = "22222";
+}
+
+console.log(myVariable); ///22222 is printed, doenst happen with let or var
+/*
 muliline
-comment 
+comment
 */

81cdc71d1f784cc8a90c64cef788780a822efde9
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-03-15 11:09:01 -0600

    Add exercice second with if else

--- javascript/control-flow/conditions.js
+++ javascript/control-flow/conditions.js
@@ -51,9 +51,34 @@ if (num > 0) {
 }
 
 //check is user is more than 18
-let age = prompt("Type your age:");
-if (age >= 18) {
-  alert("Welcome");
+// let age = prompt("Type your age:");
+// if (age >= 18) {
+//   alert("Welcome");
+// } else {
+//   alert("Come back when you grew up");
+// }
+
+//exercise 2
+let num2 = 10;
+
+if (num2 > 0 && num2 > 100) {
+  console.log(`Number is positsive and greater than 100.: ${num2}`);
+} else if (num2 > 0 && num2 < 100) {
+  console.log(`Number is positive but less than 100: ${num2}`);
 } else {
-  alert("Come back when you grew up");
+  console.log("Number is negative.");
+}
+
+const score = 60;
+
+if (score >= 90) {
+  console.log("A");
+} else if (score >= 80) {
+  console.log("B");
+} else if (score >= 70) {
+  console.log("C");
+} else if (score >= 55) {
+  console.log("D");
+} else {
+  console.log("F");
 }

05718242393e50e001661b1fb9fd0d829f5c4d1f
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-03-15 09:25:23 -0600

    Scope with let

--- javascript/control-flow/conditions.js
+++ javascript/control-flow/conditions.js
@@ -11,3 +11,13 @@ console.log("Test");
 
 //will return 5 with var, if we change var to let/const it wont, let is blocked scoped
 console.log(x);
+
+//let example
+
+{
+  let z = x + 5;
+
+  {
+    console.log(z); //will work because is in the block
+  }
+}

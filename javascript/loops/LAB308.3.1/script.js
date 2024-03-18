267b9f27ddf380490d4effc9d173c955b990d011
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-03-18 14:50:30 -0600

    Add prime time exercise

--- javascript/loops/LAB308.3.1/script.js
+++ javascript/loops/LAB308.3.1/script.js
@@ -1,12 +1,27 @@
 //FizzBuzz
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log("Fizz Buzz");
+    //console.log("Fizz Buzz");
   } else if (i % 3 === 0) {
-    console.log("Fizz");
+    //console.log("Fizz");
   } else if (i % 5 === 0) {
-    console.log("Buzz");
+    //console.log("Buzz");
   } else {
-    console.log(i);
+    // console.log(i);
+  }
+}
+
+//prime time
+
+for (let n = 12; n < 20; n++) {
+  let isPrime = true;
+  for (let j = 2; j < n; j++) {
+    if (n % j == 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    console.log(n);
+    break;
   }
 }

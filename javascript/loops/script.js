f11acd60e3956c8d4b8b700dbc93e845eabf18c2
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-03-18 10:35:48 -0600

    Add for loop

---
+++ javascript/loops/script.js
@@ -0,0 +1,45 @@
+for (let i = 0; i < 10; i++) {
+  //console.log(i);
+}
+
+//exercise 1
+
+//for loop
+//count down from  10 to 1
+for (let i = 10; i >= 1; i--) {
+  console.log(i);
+}
+
+//output odd and even numbers from 1 to 10
+const odd = [];
+const even = [];
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 == 0) {
+    even.push(i);
+  } else {
+    odd.push(i);
+  }
+}
+console.log(`Odd numbers: ${odd}`);
+console.log(`Even numbers: ${even}`);
+
+//output mulitiple of 3, start at 6 end at 60;
+let numbers = [];
+for (let i = 6; i <= 60; i += 3) {
+  numbers.push(i);
+}
+console.log("Numbers multiplies by 3: " + numbers);
+
+/// output hashs
+let sum = "";
+for (let i = 1; i <= 7; i++) {
+  sum += "#";
+  console.log(sum);
+}
+
+//iterates from 1 to 20
+for (let i = 2; i <= i ** 2; i++) {
+  if (i % i == 0) {
+    console.log("Prime: " + i);
+  }
+}

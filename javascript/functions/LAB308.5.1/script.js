bb2fa35ce8eaaca3bf5027666bfe4184007d810b
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-03-21 15:38:49 -0600

    Add Part 2

--- javascript/functions/LAB308.5.1/script.js
+++ javascript/functions/LAB308.5.1/script.js
@@ -62,3 +62,45 @@ function printNumbers(n) {
   printNumbers(n - 1);
 }
 printNumbers(4);
+
+///PART 2
+const array = [
+  { id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+  { id: "48", name: "Barry", occupation: "Runner", age: "25" },
+  { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
+  { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
+  { id: "7", name: "Bilbo", occupation: "None", age: "111" },
+];
+
+//sort arr by age
+let sortedArr = array.sort((prev, next) => prev.age - next.age);
+console.log("Sorted array by age: ");
+console.log(sortedArr);
+
+//remove entries with age greater than 50
+let newArr = array.filter((obj) => {
+  return obj.age < 50;
+});
+console.log(`\n`);
+console.log("Array with entries that age is less than 50: ");
+console.log(newArr);
+
+//map array change 'occupation' to job
+let changedArr = newArr.map((obj) => ({
+  id: obj.id,
+  name: obj.name,
+  job: obj.occupation,
+  age: String(Number(obj.age) + 1),
+}));
+console.log(`\n`);
+console.log("Array with changed 'occupation' to 'job' and age + 1");
+console.log(changedArr);
+
+//reduce to calculate the sum of ages
+let sumOfAge = changedArr.reduce(sumAge, 0);
+function sumAge(sum, obj) {
+  return sum + Number(obj.age);
+}
+console.log("Sum of all age: " + sumOfAge);
+//calc avg
+console.log(`Avarage age   ${Math.round(sumOfAge / changedArr.length)}`);

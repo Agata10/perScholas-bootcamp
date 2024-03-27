fa682b163c256707d6867b0ed8a748f8549d596a
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-03-27 15:48:04 -0600

    Add Part 3

--- javascript/DOM/ALAB316.1.1/index.js
+++ javascript/DOM/ALAB316.1.1/index.js
@@ -1,4 +1,10 @@
-import "./styles.css";
+// Menu data structure
+var menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
 
 //PART 1
 const mainEl = document.querySelector("main");
@@ -11,3 +17,11 @@ const topMenuEl = document.getElementById("top-menu");
 topMenuEl.style.height = "100%";
 topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
 topMenuEl.classList.add("flex-around");
+
+//PART 3
+menuLinks.forEach((m) => {
+  const link = document.createElement("a");
+  link.setAttribute("href", m.href);
+  link.textContent = m.text;
+  topMenuEl.appendChild(link);
+});

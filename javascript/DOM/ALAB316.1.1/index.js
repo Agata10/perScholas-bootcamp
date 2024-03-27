9d3849bac1210f3e29a47fd18b1aadb2c40aad79
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-03-27 15:26:18 -0600

    Add Part 2

--- javascript/DOM/ALAB316.1.1/index.js
+++ javascript/DOM/ALAB316.1.1/index.js
@@ -1,6 +1,13 @@
 import "./styles.css";
 
+//PART 1
 const mainEl = document.querySelector("main");
 mainEl.style.backgroundColor = "var(--main-bg)";
 mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
 mainEl.classList.add("flex-ctr");
+
+//PART 2
+const topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");

3fc94da6b95e5bdf476daaab5f6dbd5a0e54d79c
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-03-29 16:37:59 -0600

    Add DOM part 2 solution to ALAB 316.3.1

--- javascript/DOM/ALAB316.1.1/index.js
+++ javascript/DOM/ALAB316.1.1/index.js
@@ -1,9 +1,32 @@
 // Menu data structure
 var menuLinks = [
   { text: "about", href: "/about" },
-  { text: "catalog", href: "/catalog" },
-  { text: "orders", href: "/orders" },
-  { text: "account", href: "/account" },
+  {
+    text: "catalog",
+    href: "#",
+    subLinks: [
+      { text: "all", href: "/catalog/all" },
+      { text: "top selling", href: "/catalog/top" },
+      { text: "search", href: "/catalog/search" },
+    ],
+  },
+  {
+    text: "orders",
+    href: "#",
+    subLinks: [
+      { text: "new", href: "/orders/new" },
+      { text: "pending", href: "/orders/pending" },
+      { text: "history", href: "/orders/history" },
+    ],
+  },
+  {
+    text: "account",
+    href: "#",
+    subLinks: [
+      { text: "profile", href: "/account/profile" },
+      { text: "sign out", href: "/account/signout" },
+    ],
+  },
 ];
 
 //PART 1
@@ -34,3 +57,65 @@ subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
 subMenuEl.classList.add("flex-around");
 subMenuEl.style.position = "absolute";
 subMenuEl.style.top = "0";
+
+const topMenuLinks = document.querySelectorAll("a");
+
+topMenuEl.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (event.target.tagName !== "A") {
+    return;
+  }
+
+  //clear subMenu if clicked again same top link
+  subMenuEl.replaceChildren();
+  event.target.classList.toggle("active");
+  console.log(event.target);
+  topMenuLinks.forEach((anchor) => {
+    if (anchor !== event.target) {
+      anchor.classList.remove("active");
+    }
+  });
+  // console.log(event.target);
+  const link = menuLinks.find((obj) => {
+    if (obj.text === event.target.text) {
+      return true;
+    }
+  });
+
+  console.log(link);
+
+  if (link.hasOwnProperty("subLinks")) {
+    buildSubMenu(link.subLinks);
+    subMenuEl.style.top = "100%";
+  } else {
+    subMenuEl.style.top = "0";
+  }
+});
+
+function buildSubMenu(array) {
+  array.forEach((subLink) => {
+    const a = document.createElement("a");
+    a.setAttribute("href", subLink.href);
+    a.textContent = subLink.text;
+    subMenuEl.appendChild(a);
+  });
+}
+
+subMenuEl.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (e.target.tagName !== "A") {
+    return;
+  }
+
+  subMenuEl.style.top = "0";
+
+  topMenuLinks.forEach((anchor) => {
+    if (anchor !== e.target) {
+      anchor.classList.remove("active");
+    }
+  });
+
+  mainEl.innerHTML = `<h1>${e.target.text}</h1>`;
+});

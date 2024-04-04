4ddc264dab37ccb26125f4d424ea4c53fd2c2681
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-04-04 14:41:44 -0600

    Add part 2

--- javascript/call-stack/script.js
+++ javascript/call-stack/script.js
@@ -7,6 +7,45 @@ function increment() {
 try {
   increment();
 } catch (err) {
-  console.log(err);
-  console.log(counter); //11026
+  // console.log(err);
+  // console.log(counter); //11026
 }
+
+///PART 2
+const flat = (array) => {
+  let result = [];
+  for (let index in array) {
+    if (Array.isArray(array[index])) {
+      result = result.concat(flat(array[index]));
+      flat(array[index]);
+    } else {
+      result.push(array[index]);
+    }
+  }
+  return result;
+};
+
+// function flatten(items) {
+//     const flat = [];
+
+//     items.forEach(item => {
+//       if (Array.isArray(item)) {
+//         flat.push(...flatten(item));
+//       } else {
+//         flat.push(item);
+//       }
+//     });
+
+//     return flat;
+//   }
+
+console.log(flat([1, [2, 3, [4, 5, 6, [7, 8, 9]]]]));
+const trampoline = (f, ...arg) => {
+  let result = f(...arg);
+  while (typeof result === "function") {
+    result = result();
+  }
+  return result();
+};
+
+console.log(trampoline([1, [2, 3, [4, 5]]]));

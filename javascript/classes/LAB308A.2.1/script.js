8a6ade1725e604ea6dbd620e38c12e96c6937dcb
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-04-05 14:57:08 -0600

    Add Part 1

---
+++ javascript/classes/LAB308A.2.1/script.js
@@ -0,0 +1,29 @@
+//PART 1
+const adventurer = {
+  name: "Robin",
+  health: 10,
+  inventory: ["sword", "potion", "artifact"],
+  companion: {
+    name: "Leo",
+    type: "Cat",
+    companion: {
+      name: "Frank",
+      type: "Flea",
+      inventory: ["small hat", "sunglasses"],
+    },
+  },
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+  },
+};
+
+for (let inventory of adventurer.inventory) {
+  console.log(inventory);
+}
+
+adventurer.roll();
+adventurer.roll();
+adventurer.roll();
+
+//PART 2

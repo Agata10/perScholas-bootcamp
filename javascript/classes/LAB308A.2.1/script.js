7e665720e69aaa3df0c332048606b65d6f1c3821
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-04-05 15:45:50 -0600

    Part 4

--- javascript/classes/LAB308A.2.1/script.js
+++ javascript/classes/LAB308A.2.1/script.js
@@ -33,11 +33,64 @@ class Character {
     this.health = 100;
     this.inventory = [];
   }
+  static MAX_HEALTH = 100;
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+  }
 }
-const robin = new Character("Robin");
-robin.inventory = ["sword", "potion", "artifact"];
-robin.companion = new Character("Leo");
-robin.companion.type = "Cat";
-robin.companion.companion = new Character("Frank");
-robin.companion.companion.type = "Flea";
-robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+// const robin = new Character("Robin");
+// robin.inventory = ["sword", "potion", "artifact"];
+// robin.companion = new Character("Leo");
+// robin.companion.type = "Cat";
+// robin.companion.companion = new Character("Frank");
+// robin.companion.companion.type = "Flea";
+// robin.companion.companion.inventory = ["small hat", "sunglasses"];
+//console.log(robin);
+
+//PART 3
+class Adventurer extends Character {
+  static ROLES = ["Fighter", "Healer", "Wizard"];
+  constructor(name, role, power) {
+    super(name);
+    const found = Adventurer.ROLES.find((r) => {
+      if (role === r) {
+        return true;
+      }
+    });
+    if (found) {
+      this.role = role;
+    } else {
+      throw new Error("Ups, not such role.");
+    }
+    this.role = role;
+    this.power = power;
+    // Every adventurer starts with a bed and 50 gold coins.
+    this.inventory.push("bedroll", "50 gold coins");
+  }
+
+  // Adventurers have the ability to scout ahead of them.
+  scout() {
+    console.log(`${this.name} is scouting ahead...`);
+    super.roll();
+  }
+}
+
+class Companion extends Character {
+  constructor(name, type) {
+    super(name);
+    this.type = type;
+  }
+  talk() {
+    console.log(`${this.name} of ${this.type} keep talking`);
+  }
+}
+
+const robin1 = new Adventurer("Robin", "Fighter", 500);
+const companion1 = new Companion("Leo", "Cat");
+companion1.inventory = ["sword", "potion", "artifact"];
+const companion2 = new Companion("Frank", "Flea");
+companion2.inventory = ["small hat", "sunglasses"];
+console.log(Adventurer.ROLES);
+console.log(companion2);

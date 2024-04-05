0c8d791618725c24fe1f759987494c78d61c04a3
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-04-05 15:04:00 -0600

    Add Part 2

--- javascript/classes/LAB308A.2.1/script.js
+++ javascript/classes/LAB308A.2.1/script.js
@@ -27,3 +27,17 @@ adventurer.roll();
 adventurer.roll();
 
 //PART 2
+class Character {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.inventory = [];
+  }
+}
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];

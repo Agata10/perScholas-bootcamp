622e0992973dbf6ec59d50579ca1d8da57156a8b
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-04-11 09:11:45 -0600

    Add morining exercise with objects/classes

---
+++ javascript/promises/objects-morning-exercise/script.js
@@ -0,0 +1,15 @@
+/*
+Objective: We are going to create an object Radio!
+How to begin:
+1. Create a variable called radio and assign as its value an object literal
+2. Give it a property called stations and make the value an array of
+station objects (at least 2)
+3. Give each station object a property called name and songs. Name
+should be a string and songs should be an array of song objects (at
+least 3).
+4. Song objects should have two properties: title and artist
+5. Create a method on your radio object that changes the station
+randomly (research Math.random and Math.floor)
+6. When the station is changed, pick a song from that station. Example:
+console.log("Now Playing:" + song.title + "by" + song.artist)
+*/

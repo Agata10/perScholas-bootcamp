3f03f7ba2b0a71b82737e2846441d513ef9e391a
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-04-18 14:42:53 -0600

    Add lab with node

---
+++ node/GLAB318.1/index.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+
+  //res.end("Hello World!\n");
+
+  switch (req.url) {
+    case "/":
+      res.setHeader("Content-type", "text/html");
+      res.write('<h1 style="color: red">Hello World!</h1>');
+      res.write("<p>I wonder what else we can send...</p>");
+      break;
+    case "/menu":
+      res.setHeader("Content-type", "text/plain");
+      res.write("MENU");
+      break;
+    default:
+      res.statusCode = 404;
+      res.write("Error: 404 Not Found");
+      break;
+  }
+  res.end();
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server runniing at http://${hostname}:${port}/`);
+});

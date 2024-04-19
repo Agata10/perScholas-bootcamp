94c4db69c126070aebd36bc39c3b13f5bb84ca37
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-04-19 10:02:29 -0600

    add different req methods and route parameters

--- express/index.js
+++ express/index.js
@@ -2,6 +2,25 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("Hello, from server");
+  console.log("got here");
+});
+
+app.get("/express", (req, res) => {
+  res.send("Hello, got to route /express");
+});
+
+//route parameters
+app.get("/users/:userID/profile/:data", (req, res) => {
+  console.log(req.params);
+  res.send("Recieved  GET request at /users");
+});
+
+app.post("/users", (req, res) => {
+  res.send("Recieved  POST request at /users");
+});
+
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}.`);
 });

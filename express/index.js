03c3c3bbadd81e8c6f10f0751e7f2e94b2a67676
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-04-19 13:18:31 -0600

    Add template engine

--- express/index.js
+++ express/index.js
@@ -28,6 +28,50 @@ const port = 3000;
 const userRoutes = require("./routes/user");
 app.use("/user", userRoutes);
 
+// serve static files from the styles directory
+app.use(express.static("./styles"));
+
+// require the filesystem module
+const fs = require("fs");
+// define the template engine
+app.engine("perscholas", (filePath, options, callback) => {
+  fs.readFile(filePath, (err, content) => {
+    if (err) return callback(err);
+
+    // Here, we take the content of the template file,
+    // convert it to a string, and replace sections of
+    // it with the values being passed to the engine.
+    const rendered = content
+      .toString()
+      .replaceAll("#title#", `${options.title}`)
+      .replace("#content#", `${options.content}`);
+    return callback(null, rendered);
+  });
+});
+
+app.set("views", "./views"); // specify the views directory
+app.set("view engine", "perscholas"); // register the template engine
+
+app.get("/", (req, res) => {
+  const options = {
+    title: "Rendering Views with Express",
+    content:
+      "Here, we've created a basic template engine using <code>app.engine()</code> \
+      and the <code>fs</code> module, then used <code>res.render</code> to \
+      render this page using custom content within the template.<br><br> \
+      Generally, you won't want to create your own view engines, \
+      but it important to understand how they work behind the scenes. \
+      For a look at some popular view engines, check out the documentation for \
+      <a href='https://pugjs.org/api/getting-started.html'>Pug</a>, \
+      <a href='https://www.npmjs.com/package/mustache'>Mustache</a>, or \
+      <a href='https://www.npmjs.com/package/ejs'>EJS</a>. \
+      More complete front-end libraries like React, Angular, and Vue \
+      also have Express integrations.",
+  };
+
+  res.render("index", options);
+});
+
 //MIDDLEWARE
 // const logReq = (req, res, next) => {
 //   const check = true;
@@ -42,10 +86,6 @@ app.use("/user", userRoutes);
 // };
 
 // app.use(logReq);
-
-app.get("/", (req, res) => {
-  res.send("Keeping it simple.");
-});
 
 // app.get("/", (req, res) => {
 //   res.send("Hello, from server");
@@ -73,6 +113,10 @@ app.get("/", (req, res) => {
 //   })
 //   .post((req, res) => {
 //     res.send("Recieved  POST request at /users");
+//   });
+
+// app.get("/", (req, res) => {
+//     res.send("Keeping it simple.");
 //   });
 
 app.listen(port, () => {

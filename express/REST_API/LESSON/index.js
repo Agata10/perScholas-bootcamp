b081e45ff14301c3db5a817ea7496673393f5707
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-04-22 11:17:09 -0600

    Add route for users

--- express/REST_API/LESSON/index.js
+++ express/REST_API/LESSON/index.js
@@ -1,14 +1,47 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = 3000;
 
-const users = require("./data/users");
-const posts = require("./data/posts");
+const users = require('./data/users');
+const posts = require('./data/posts');
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Got it");
+app.get('/', (req, res) => {
+  res.send('Got it');
+});
+
+app
+  .route('/api/users')
+  .get((req, res) => {
+    res.json(users);
+  })
+  .post((req, res) => {
+    console.log('req.body ' + req.body);
+    res.json('Post /api/users');
+  });
+
+app.get('/api/users/:id', (req, res, next) => {
+  const user = users.find((u) => u.id == req.params.id);
+  if (user) res.json(user);
+  next();
+});
+
+app.get('/api/posts', (req, res) => {
+  res.json(posts);
+});
+
+app.get('/api/posts/:id', (req, res, next) => {
+  const post = posts.find((post) => post.id == req.params.id);
+  if (post) res.json(post);
+  next();
+});
+
+//error handling
+app.use((req, res) => {
+  res.status(404);
+  res.json({ error: 'Resource not found' });
 });
 
 app.listen(PORT, () => {
-  console.log("Hello");
+  console.log('Listening on port ' + PORT);
 });

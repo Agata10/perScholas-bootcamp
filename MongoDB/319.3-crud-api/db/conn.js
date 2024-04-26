6606e0d1d2de1dd21ebb26f3c552d5a071c7f2ff
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-04-26 13:52:55 -0600

    Add get for signle student grades data and for single class data

--- MongoDB/319.3-crud-api/db/conn.js
+++ MongoDB/319.3-crud-api/db/conn.js
@@ -6,6 +6,7 @@ const client = new MongoClient(connectionString);
 let conn;
 try {
   conn = await client.connect();
+  console.log("Connected to MongoDB");
 } catch (err) {
   console.log(err);
 }
--- MongoDB/319.3-crud-api/routes/grades.js
+++ MongoDB/319.3-crud-api/routes/grades.js
@@ -1,7 +1,41 @@
 import express from "express";
+import db from "../db/conn.js";
+import { ObjectId } from "mongodb";
 const router = express.Router();
 
-router.route("/").get((req, res) => {
-  res.send("grades");
+router.route("/").get(async (req, res) => {
+  let collection = db.collection("grades");
+  let result = await collection.find().limit(4).toArray();
+  res.send(result).status(200);
+});
+
+//get a single grade by id
+router.get("/:id", async (req, res) => {
+  let collection = await db.collection("grades");
+  let query = { _id: new ObjectId(req.params.id) };
+  let result = await collection.findOne(query);
+
+  if (!result) res.send("Not found").status(404);
+  res.send(result).status(200);
+});
+
+// Get a single student grade data
+router.get("/learner/:id", async (req, res) => {
+  let collection = await db.collection("grades");
+  let query = { learner_id: Number(req.params.id) };
+  let result = await collection.find(query).toArray();
+
+  if (!result) res.send("Not found").status(404);
+  res.send(result).status(200);
+});
+
+// Get a single class grade data
+router.get("/class/:id", async (req, res) => {
+  let collection = await db.collection("grades");
+  let query = { class_id: Number(req.params.id) };
+  let result = await collection.find(query).toArray();
+
+  if (!result) res.send("Not found").status(404);
+  res.send(result).status(200);
 });
 export default router;

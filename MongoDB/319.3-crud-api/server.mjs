7e63bacb0f9cf6c39a04d04885725224f195b291
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-04-26 11:13:08 -0600

    Make server listening

--- MongoDB/319.3-crud-api/server.mjs
+++ MongoDB/319.3-crud-api/server.mjs
@@ -5,4 +5,11 @@ const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(express.json());
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.send("Welcome to API");
+});
+
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});

5960bd8bf687ff9888ff09199ab57c7465295066
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-04-26 11:30:42 -0600

    Add grades routes

--- MongoDB/319.3-crud-api/server.mjs
+++ MongoDB/319.3-crud-api/server.mjs
@@ -1,10 +1,12 @@
 import express from "express";
 import "dotenv/config";
+import gradesRoutes from "./routes/grades.mjs";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(express.json());
+app.use("/grades", gradesRoutes);
 app.get("/", (req, res) => {
   res.send("Welcome to API");
 });

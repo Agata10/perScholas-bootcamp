5157cd13c1633a06e51fd6f4f5c212c751f73b47
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-04-29 19:43:17 -0600

    Add first part to ALAB319.4

--- MongoDB/ALAB-319.4/server.js
+++ MongoDB/ALAB-319.4/server.js
@@ -80,6 +80,7 @@ app.get('/grades', async (req, res) => {
   res.send(result).status(204);
 });
 
+//grades statistics
 app.get('/grades/stats', async (req, res) => {
   let collection = await db.collection('grades');
 
@@ -87,19 +88,191 @@ app.get('/grades/stats', async (req, res) => {
     .aggregate([
       {
         $project: {
+          _id: 0,
+          learner_id: 1,
+          class_id: 1,
           avg: {
             $avg: '$scores.score',
           },
         },
-        $match: {
-          avg: { $gt: 70 },
+      },
+      {
+        $group: {
+          _id: '$learner_id',
+          avg: {
+            $avg: '$avg',
+          },
         },
-        $count: 'numberOfDoc',
+      },
+      {
+        $group: {
+          _id: null,
+          totalLearners: {
+            $sum: 1,
+          },
+          learnersAbove60: {
+            $sum: {
+              $cond: [
+                {
+                  $gt: ['$avg', 60],
+                },
+                1,
+                0,
+              ],
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          totalLearners: 1,
+          learnersAbove60: 1,
+          percentageOfLearnersWith70Score: {
+            $multiply: [
+              { $divide: ['$learnersAbove60', '$totalLearners'] },
+              100,
+            ],
+          },
+        },
       },
     ])
     .toArray();
   res.send(result);
 });
+
+//grades statistics for a class
+app.get('/grades/stats/:id', async (req, res) => {
+  let collection = await db.collection('grades');
+
+  let result = await collection
+    .aggregate([
+      {
+        $match: { class_id: Number(req.params.id) },
+      },
+      {
+        $project: {
+          _id: 0,
+          learner_id: 1,
+          class_id: 1,
+          avg: {
+            $avg: '$scores.score',
+          },
+        },
+      },
+      {
+        $group: {
+          _id: '$learner_id',
+          avg: {
+            $avg: '$avg',
+          },
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          totalLearners: {
+            $sum: 1,
+          },
+          learnersAbove60: {
+            $sum: {
+              $cond: [
+                {
+                  $gt: ['$avg', 60],
+                },
+                1,
+                0,
+              ],
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          totalLearners: 1,
+          learnersAbove60: 1,
+          percentageOfLearnersWith70Score: {
+            $multiply: [
+              { $divide: ['$learnersAbove60', '$totalLearners'] },
+              100,
+            ],
+          },
+        },
+      },
+    ])
+    .toArray();
+  res.send(result);
+});
+
+/**
+ * Grading Weights by Score Type:
+ * - Exams: 50%
+ * - Quizes: 30%
+ * - Homework: 20%
+ */
+
+// // Get the weighted average of a specified learner's grades, per class
+// router.get('/learner/:id/avg-class', async (req, res) => {
+//   let collection = await db.collection('grades');
+
+//   let result = await collection
+//     .aggregate([
+//       {
+//         $match: { learner_id: Number(req.params.id) },
+//       },
+//       {
+//         $unwind: { path: '$scores' },
+//       },
+//   {
+//     $group: {
+//       _id: '$class_id',
+//       quiz: {
+//         $push: {
+//           $cond: {
+//             if: { $eq: ['$scores.type', 'quiz'] },
+//             then: '$scores.score',
+//             else: '$$REMOVE',
+//           },
+//         },
+//       },
+//       exam: {
+//         $push: {
+//           $cond: {
+//             if: { $eq: ['$scores.type', 'exam'] },
+//             then: '$scores.score',
+//             else: '$$REMOVE',
+//           },
+//         },
+//       },
+//       homework: {
+//         $push: {
+//           $cond: {
+//             if: { $eq: ['$scores.type', 'homework'] },
+//             then: '$scores.score',
+//             else: '$$REMOVE',
+//           },
+//         },
+//       },
+//     },
+//   },
+//   {
+//     $project: {
+//       _id: 0,
+//       class_id: '$_id',
+//       avg: {
+//         $sum: [
+//           { $multiply: [{ $avg: '$exam' }, 0.5] },
+//           { $multiply: [{ $avg: '$quiz' }, 0.3] },
+//           { $multiply: [{ $avg: '$homework' }, 0.2] },
+//         ],
+//       },
+//     },
+//   },
+//     ])
+//     .toArray();
+
+//   if (!result) res.send('Not found').status(404);
+//   else res.send(result).status(200);
+// });
 
 // Global error handling
 app.use((err, _req, res, next) => {

eb6d6b333702549eb41fffc35a098fa53947cfba
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-01 13:36:22 -0600

    Add code to alab 319.5

---
+++ MongoDB/319.5.1-mongoose/routes/grades.js
@@ -0,0 +1,141 @@
+import express from 'express';
+import db from '../db/conn.js';
+import { ObjectId } from 'mongodb';
+const router = express.Router();
+
+router.route('/').get(async (req, res) => {
+  let collection = await db.collection('grades');
+  let result = await collection.find().limit(4).toArray();
+  if (!result) res.send('Not found').status(404);
+  res.send(result).status(200);
+});
+
+//create a single grade document
+router.post('/', async (req, res) => {
+  let collection = await db.collection('grades');
+  let newDocument = req.body;
+  if (newDocument.student_id) {
+    newDocument.learner_id = newDocument.student_id;
+    delete newDocument.student_id;
+  }
+
+  let result = await collection.insertOne(newDocument);
+  res.send(result).status(204);
+});
+
+//////////////////////////////////////////
+
+//get a single grade by id
+router.get('/:id', async (req, res) => {
+  let collection = await db.collection('grades');
+  let query = { _id: new ObjectId(req.params.id) };
+  let result = await collection.findOne(query);
+
+  if (!result) res.send('Not found').status(404);
+  res.send(result).status(200);
+});
+
+// Add a score to a grade entry
+router.patch('/:id/add', async (req, res) => {
+  let collection = await db.collection('grades');
+  let query = { _id: new ObjectId(req.params.id) };
+
+  let result = await collection.updateOne(query, {
+    $push: { scores: req.body },
+  });
+
+  if (!result) res.send('Not found').status(404);
+  else res.send(result).status(200);
+});
+
+// Remove a score from a grade entry
+router.patch('/:id/remove', async (req, res) => {
+  let collection = await db.collection('grades');
+  let query = { _id: new ObjectId(req.params.id) };
+
+  let result = await collection.updateOne(query, {
+    $pull: { scores: req.body },
+  });
+
+  if (!result) res.send('Not found').status(404);
+  else res.send(result).status(200);
+});
+
+// Delete a single grade entry
+router.delete('/:id', async (req, res) => {
+  let collection = await db.collection('grades');
+  let query = { _id: new ObjectId(req.params.id) };
+  let result = await collection.deleteOne(query);
+
+  if (!result) res.send('Not found').status(404);
+  else res.send(result).status(200);
+});
+
+////////////////////////////
+
+// Student route for backwards compatibility
+router.get('/student/:id', async (req, res) => {
+  res.redirect(`../learner/${req.params.id}`);
+});
+
+// Get a single student grade data
+router.get('/learner/:id', async (req, res) => {
+  let collection = await db.collection('grades');
+  let query = { learner_id: Number(req.params.id) };
+
+  // Check for class_id parameter
+  if (req.query.class) query.class_id = Number(req.query.class);
+
+  let result = await collection.find(query).toArray();
+
+  if (!result) res.send('Not found').status(404);
+  res.send(result).status(200);
+});
+
+// Delete a learner's grade data
+router.delete('/learner/:id', async (req, res) => {
+  let collection = await db.collection('grades');
+  let query = { learner_id: Number(req.params.id) };
+
+  let result = await collection.deleteOne(query);
+
+  if (!result) res.send('Not found').status(404);
+  else res.send(result).status(200);
+});
+
+////////////////////////////////////
+
+// Get a single class grade data
+router.get('/class/:id', async (req, res) => {
+  let collection = await db.collection('grades');
+  let query = { class_id: Number(req.params.id) };
+  let result = await collection.find(query).toArray();
+
+  if (!result) res.send('Not found').status(404);
+  res.send(result).status(200);
+});
+
+// Update a class id
+router.patch('/class/:id', async (req, res) => {
+  let collection = await db.collection('grades');
+  let query = { class_id: Number(req.params.id) };
+
+  let result = await collection.updateMany(query, {
+    $set: { class_id: req.body.class_id },
+  });
+
+  if (!result) res.send('Not found').status(404);
+  else res.send(result).status(200);
+});
+
+// Delete a class
+router.delete('/class/:id', async (req, res) => {
+  let collection = await db.collection('grades');
+  let query = { class_id: Number(req.params.id) };
+
+  let result = await collection.deleteMany(query);
+
+  if (!result) res.send('Not found').status(404);
+  else res.send(result).status(200);
+});
+export default router;
---
+++ MongoDB/319.5.1-mongoose/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import 'dotenv/config';
+import gradesRoutes from './routes/grades.js';
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(express.json());
+app.use('/grades', gradesRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to API');
+});
+
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});

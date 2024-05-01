a3dd47706f658b8f4378567be94ec416de144735
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-01 09:20:50 -0600

    Add grades 319.3 crud api

--- MongoDB/319.3-crud-api/routes/grades.js
+++ MongoDB/319.3-crud-api/routes/grades.js
@@ -1,17 +1,18 @@
-import express from "express";
-import db from "../db/conn.js";
-import { ObjectId } from "mongodb";
+import express from 'express';
+import db from '../db/conn.js';
+import { ObjectId } from 'mongodb';
 const router = express.Router();
 
-router.route("/").get(async (req, res) => {
-  let collection = db.collection("grades");
+router.route('/').get(async (req, res) => {
+  let collection = await db.collection('grades');
   let result = await collection.find().limit(4).toArray();
+  if (!result) res.send('Not found').status(404);
   res.send(result).status(200);
 });
 
 //create a single grade document
-router.post("/", async (req, res) => {
-  let collection = await db.collection("grades");
+router.post('/', async (req, res) => {
+  let collection = await db.collection('grades');
   let newDocument = req.body;
   if (newDocument.student_id) {
     newDocument.learner_id = newDocument.student_id;
@@ -25,61 +26,61 @@ router.post("/", async (req, res) => {
 //////////////////////////////////////////
 
 //get a single grade by id
-router.get("/:id", async (req, res) => {
-  let collection = await db.collection("grades");
+router.get('/:id', async (req, res) => {
+  let collection = await db.collection('grades');
   let query = { _id: new ObjectId(req.params.id) };
   let result = await collection.findOne(query);
 
-  if (!result) res.send("Not found").status(404);
+  if (!result) res.send('Not found').status(404);
   res.send(result).status(200);
 });
 
 // Add a score to a grade entry
-router.patch("/:id/add", async (req, res) => {
-  let collection = await db.collection("grades");
+router.patch('/:id/add', async (req, res) => {
+  let collection = await db.collection('grades');
   let query = { _id: new ObjectId(req.params.id) };
 
   let result = await collection.updateOne(query, {
     $push: { scores: req.body },
   });
 
-  if (!result) res.send("Not found").status(404);
+  if (!result) res.send('Not found').status(404);
   else res.send(result).status(200);
 });
 
 // Remove a score from a grade entry
-router.patch("/:id/remove", async (req, res) => {
-  let collection = await db.collection("grades");
+router.patch('/:id/remove', async (req, res) => {
+  let collection = await db.collection('grades');
   let query = { _id: new ObjectId(req.params.id) };
 
   let result = await collection.updateOne(query, {
     $pull: { scores: req.body },
   });
 
-  if (!result) res.send("Not found").status(404);
+  if (!result) res.send('Not found').status(404);
   else res.send(result).status(200);
 });
 
 // Delete a single grade entry
-router.delete("/:id", async (req, res) => {
-  let collection = await db.collection("grades");
+router.delete('/:id', async (req, res) => {
+  let collection = await db.collection('grades');
   let query = { _id: new ObjectId(req.params.id) };
   let result = await collection.deleteOne(query);
 
-  if (!result) res.send("Not found").status(404);
+  if (!result) res.send('Not found').status(404);
   else res.send(result).status(200);
 });
 
 ////////////////////////////
 
 // Student route for backwards compatibility
-router.get("/student/:id", async (req, res) => {
+router.get('/student/:id', async (req, res) => {
   res.redirect(`../learner/${req.params.id}`);
 });
 
 // Get a single student grade data
-router.get("/learner/:id", async (req, res) => {
-  let collection = await db.collection("grades");
+router.get('/learner/:id', async (req, res) => {
+  let collection = await db.collection('grades');
   let query = { learner_id: Number(req.params.id) };
 
   // Check for class_id parameter
@@ -87,54 +88,54 @@ router.get("/learner/:id", async (req, res) => {
 
   let result = await collection.find(query).toArray();
 
-  if (!result) res.send("Not found").status(404);
+  if (!result) res.send('Not found').status(404);
   res.send(result).status(200);
 });
 
 // Delete a learner's grade data
-router.delete("/learner/:id", async (req, res) => {
-  let collection = await db.collection("grades");
+router.delete('/learner/:id', async (req, res) => {
+  let collection = await db.collection('grades');
   let query = { learner_id: Number(req.params.id) };
 
   let result = await collection.deleteOne(query);
 
-  if (!result) res.send("Not found").status(404);
+  if (!result) res.send('Not found').status(404);
   else res.send(result).status(200);
 });
 
 ////////////////////////////////////
 
 // Get a single class grade data
-router.get("/class/:id", async (req, res) => {
-  let collection = await db.collection("grades");
+router.get('/class/:id', async (req, res) => {
+  let collection = await db.collection('grades');
   let query = { class_id: Number(req.params.id) };
   let result = await collection.find(query).toArray();
 
-  if (!result) res.send("Not found").status(404);
+  if (!result) res.send('Not found').status(404);
   res.send(result).status(200);
 });
 
 // Update a class id
-router.patch("/class/:id", async (req, res) => {
-  let collection = await db.collection("grades");
+router.patch('/class/:id', async (req, res) => {
+  let collection = await db.collection('grades');
   let query = { class_id: Number(req.params.id) };
 
   let result = await collection.updateMany(query, {
     $set: { class_id: req.body.class_id },
   });
 
-  if (!result) res.send("Not found").status(404);
+  if (!result) res.send('Not found').status(404);
   else res.send(result).status(200);
 });
 
 // Delete a class
-router.delete("/class/:id", async (req, res) => {
-  let collection = await db.collection("grades");
+router.delete('/class/:id', async (req, res) => {
+  let collection = await db.collection('grades');
   let query = { class_id: Number(req.params.id) };
 
   let result = await collection.deleteMany(query);
 
-  if (!result) res.send("Not found").status(404);
+  if (!result) res.send('Not found').status(404);
   else res.send(result).status(200);
 });
 export default router;

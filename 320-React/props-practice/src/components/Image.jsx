a8d52db0b463216e96e3cd3d6f9cf4be26efe15f
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-06 11:50:43 -0600

    add comment to props destructing

--- 320-React/props-practice/src/components/Image.jsx
+++ 320-React/props-practice/src/components/Image.jsx
@@ -1,6 +1,9 @@
 export default function Image({ href, src, alt }) {
   //or if passed (props)
-  //let href= props.href etc
+  //let href= props.href
+  //let src=props.src
+  //let alt=props.alt
+
   return (
     <a href={href} target="_blank">
       <img src={src} alt={alt} className="logo" />

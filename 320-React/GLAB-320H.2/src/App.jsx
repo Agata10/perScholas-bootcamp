14774483c4e97f4435e413cb6c2cd0fe0484c5b9
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-06 14:35:08 -0600

    Add GLAB-320H.2

--- 320-React/GLAB-320H.2/src/App.jsx
+++ 320-React/GLAB-320H.2/src/App.jsx
@@ -1,7 +1,25 @@
 import './App.css';
+function Header() {
+  return <h1>Simple React Application</h1>;
+}
 
+function Content(props) {
+  return <p style={{ color: props.color }}>{props.text}</p>;
+}
+
+function Footer() {
+  return <h1>Created by Me, of course</h1>;
+}
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Content color="blue" text="This is my x React App!" />
+      <Content color="red" text="Wish me luck...." />
+      <Content color="green" text="I think I've got it" />
+      <Footer />
+    </>
+  );
 }
 
 export default App;

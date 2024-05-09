d16c323202cca25e14e8841573976f7760f5fa24
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-09 14:59:23 -0600

    Add state to SearchBar

--- 320-React/product-list/src/components/SearchBar.jsx
+++ 320-React/product-list/src/components/SearchBar.jsx
@@ -1,9 +1,24 @@
-const SearchBar = ({ filterText, inStockOnly }) => {
+const SearchBar = ({
+  filterText,
+  inStockOnly,
+  setFilterText,
+  setInStockOnly,
+}) => {
   return (
     <form style={{ display: 'flex', flexDirection: 'column' }}>
-      <input type="search" placeholder="Search..." value={filterText} />
+      <input
+        type="search"
+        placeholder="Search..."
+        value={filterText}
+        onChange={(e) => setFilterText(e.target.value)}
+      />
       <label htmlFor="stock">
-        <input type="checkbox" id="stock" checked={inStockOnly} />
+        <input
+          type="checkbox"
+          id="stock"
+          checked={inStockOnly}
+          onChange={(e) => setInStockOnly(e.target.value)}
+        />
         Only show products in stocks
       </label>
     </form>

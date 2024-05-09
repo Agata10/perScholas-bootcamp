558c36656dc906b363db469e11a9cf08871cb4e1
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-09 14:12:29 -0600

    Add state to filtearableproducttable component

--- 320-React/product-list/src/components/FilterableProductTable.jsx
+++ 320-React/product-list/src/components/FilterableProductTable.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import ProductTable from './ProductTable';
 import SearchBar from './SearchBar';
 
 const FilterableProductTable = ({ products }) => {
+  const [filterText, setfilterText] = useState('');
+  const [inStockOnly, setInStockOnly] = useState('false');
   return (
     <div>
-      <SearchBar />
-      <ProductTable products={products} />
+      <SearchBar filterText={filterText} inStockOnly={inStockOnly} />
+      <ProductTable
+        products={products}
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+      />
     </div>
   );
 };

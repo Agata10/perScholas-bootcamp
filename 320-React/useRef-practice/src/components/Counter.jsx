a0d7592ee2eb752ceaeb045507f5a40b5ad2d1e7
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-10 11:20:56 -0600

    Add Counter component

---
+++ 320-React/useRef-practice/src/components/Counter.jsx
@@ -0,0 +1,15 @@
+import { useRef, useState } from 'react';
+const Counter = () => {
+  let ref = useRef(0);
+  const [counter, setCounter] = useState(0);
+  //   let ref = { current: 0 }; won't work, reset the value to 0 when each time is clicked
+  function handleClick() {
+    ref.current += 1;
+    setCounter(counter + 1);
+    alert('You clicked ' + ref.current + ' times!');
+  }
+  console.log('rendered Counter', counter);
+  return <button onClick={handleClick}>Click me!</button>;
+};
+
+export default Counter;

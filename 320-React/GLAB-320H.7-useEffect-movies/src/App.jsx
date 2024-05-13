553066f1dc4f57613300784b32d68eb22f4ecfd2
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-13 14:26:25 -0600

    Add fetching data

--- 320-React/GLAB-320H.7-useEffect-movies/src/App.jsx
+++ 320-React/GLAB-320H.7-useEffect-movies/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import 'dotenv/config';
+
 import './App.css';
 
 // Import our components
@@ -7,9 +8,22 @@ import MovieDisplay from './components/MovieDisplay';
 import Form from './components/Form';
 
 export default function App() {
+  const [movie, setMovie] = useState(null);
+  //api_key
+  const apikey = '310326df';
+  //catch movies
+  const getMovie = async (searchTerm) => {
+    const response = await fetch(
+      `http://www.omdbapi.com/?${apikey}=YOURKEY&t=${searchTerm}`
+    );
+    //parse json response into js object
+    const data = await response.json();
+    //set movie
+    setMovie(data);
+  };
   return (
     <div className="App">
-      <Form />
+      <Form moviesearch={getMovie} />
       <MovieDisplay />
     </div>
   );

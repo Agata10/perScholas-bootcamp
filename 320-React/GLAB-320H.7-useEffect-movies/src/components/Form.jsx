7c1b7f6d4c193a9839aa476da36f6864bcbedab9
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-13 14:36:00 -0600

    Add from logic

--- 320-React/GLAB-320H.7-useEffect-movies/src/components/Form.jsx
+++ 320-React/GLAB-320H.7-useEffect-movies/src/components/Form.jsx
@@ -1,9 +1,26 @@
+import { useState } from 'react';
 export default function Form(props) {
-  // The component must return some JSX
+  //hold data from form
+  const [formData, setForData] = useState({
+    searchterm: '',
+  });
+  ///update state when typing into input
+  const handleChange = (event) => {
+    setForData({ ...formData, [event.target.name]: event.target.value });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.moviesearch(formData.searchterm);
+  };
   return (
     <div>
-      <form>
-        <input type="text" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="searchterm"
+          value={formData.searchterm}
+          onChange={handleChange}
+        />
         <input type="submit" value="submit" />
       </form>
     </div>

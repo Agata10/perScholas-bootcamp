296151ce492aade1d7721964fbe2e790ca8418d6
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-13 14:54:24 -0600

    Add extra bonus and catching errors

--- 320-React/GLAB-320H.7-useEffect-movies/src/App.jsx
+++ 320-React/GLAB-320H.7-useEffect-movies/src/App.jsx
@@ -10,20 +10,41 @@ export default function App() {
   const [movie, setMovie] = useState(null);
   //api_key
   const apikey = '310326df';
+
+  //getrandommovie
+  const randomMovie = () => {
+    const movies = [
+      'The Shawshank Redemption',
+      'The Godfather',
+      'The Dark Knight',
+      'Pulp Fiction',
+      'Fight Club',
+    ];
+    const indexRandom = Math.floor(Math.random() * movies.length);
+    return movies[indexRandom];
+  };
   //catch movies
   const getMovie = async (searchTerm) => {
-    const response = await fetch(
-      `http://www.omdbapi.com/?apikey=${apikey}&t=${searchTerm}`
-    );
-    //parse json response into js object
-    const data = await response.json();
-    //set movie
-    setMovie(data);
+    try {
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apikey}&t=${searchTerm}`
+      );
+      //parse json response into js object
+      const data = await response.json();
+      //set movie
+      setMovie(data);
+    } catch (err) {
+      console.error(err);
+    }
   };
+
+  useEffect(() => {
+    getMovie(randomMovie());
+  }, []);
   return (
     <div className="App">
       <Form moviesearch={getMovie} />
-      <MovieDisplay />
+      <MovieDisplay movie={movie} />
     </div>
   );
 }

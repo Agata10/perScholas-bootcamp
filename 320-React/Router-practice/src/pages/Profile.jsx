bb0c8d39ebd0cf5630aa2c734d6d53e57782c6d5
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-15 11:58:07 -0600

    Add loading and loaded

--- 320-React/Router-practice/src/pages/Profile.jsx
+++ 320-React/Router-practice/src/pages/Profile.jsx
@@ -10,14 +10,25 @@ const Profile = () => {
   useEffect(() => {
     if (!id) return;
     const getUserData = async () => {
-      const result = await fetch(`https://swapi.dev/api/people/${id}`);
-      const data = await result.json();
-      setUser(data.name);
+      try {
+        const result = await fetch(`https://swapi.dev/api/people/${id}`);
+        const data = await result.json();
+        setUser(data.name);
+      } catch (err) {
+        console.log(err);
+        setUser('No user to catch..');
+      }
     };
 
     getUserData();
-  }, []);
-  return <div>{user ? user : 'Profile'}</div>;
+  }, [id]);
+  const loaded = () => {
+    return <div>{user}</div>;
+  };
+  const loading = () => {
+    return <div>Loading....</div>;
+  };
+  return <div>{user ? loaded() : loading()}</div>;
 };
 
 export default Profile;

ee5fd34121f5dbbaf30d5b7c7a5cb5107304ef9d
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-15 14:02:26 -0600

    Add nav, currencies comoponent and main component

--- 320-React/GLAB-320H.8.1-router/src/App.jsx
+++ 320-React/GLAB-320H.8.1-router/src/App.jsx
@@ -1,8 +1,20 @@
-import { useState } from 'react';
 import './App.css';
-
+import { Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+import Currencies from './pages/Currencies';
+import Price from './pages/Price';
+import Nav from './components/Nav';
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/currencies" element={<Currencies />} />
+        <Route path="/price" element={<Price />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;

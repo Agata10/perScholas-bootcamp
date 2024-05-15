939688e4cd513c5488ecc7f5527017968b6ccbf3
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-15 11:53:03 -0600

    Add api to profile

--- 320-React/Router-practice/src/pages/Profile.jsx
+++ 320-React/Router-practice/src/pages/Profile.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Profile = () => {
   const { id } = useParams();
   console.log(id);
-  return <div>Profile</div>;
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    if (!id) return;
+    const getUserData = async () => {
+      const result = await fetch(`https://swapi.dev/api/people/${id}`);
+      const data = await result.json();
+      setUser(data.name);
+    };
+
+    getUserData();
+  }, []);
+  return <div>{user ? user : 'Profile'}</div>;
 };
 
 export default Profile;

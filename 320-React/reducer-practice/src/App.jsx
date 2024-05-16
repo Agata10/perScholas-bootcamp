bad92ebb0e40dddba208145a1347bf68c632f5ac
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-16 09:26:30 -0600

    Add reducer example

--- 320-React/reducer-practice/src/App.jsx
+++ 320-React/reducer-practice/src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
 
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  //state is the current state(before the changes)
+  //action is the infromation from dispatch
+  function reducer(state, action) {
+    console.log('state:', state);
+    console.log('dispatch:', action);
+    return state;
+  }
+  //dispatch -> gets info from dispatch and returns new state
+  const [state, dispatch] = useReducer(reducer, 0);
 
-  return <></>;
+  return (
+    <>
+      <button onClick={() => dispatch('information')}>Test</button>
+    </>
+  );
 }
 
 export default App;

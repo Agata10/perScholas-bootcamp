691ee157569b5508bec20e16477fefbfa564b582
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-16 09:35:35 -0600

    Add counter reducer

--- 320-React/reducer-practice/src/App.jsx
+++ 320-React/reducer-practice/src/App.jsx
@@ -6,8 +6,14 @@ function App() {
   //state is the current state(before the changes)
   //action is the infromation from dispatch
   function reducer(state, action) {
-    console.log('state:', state);
-    console.log('dispatch:', action);
+    if (action === 'inc') {
+      state++;
+    } else if (action === 'dec') {
+      state--;
+    } else {
+      state = 0;
+    }
+
     return state;
   }
   //dispatch -> gets info from dispatch and returns new state
@@ -15,7 +21,10 @@ function App() {
 
   return (
     <>
-      <button onClick={() => dispatch('information')}>Test</button>
+      <div>Counter: {state}</div>
+      <button onClick={() => dispatch('inc')}>Increment</button>
+      <button onClick={() => dispatch('dec')}>Decrement</button>
+      <button onClick={() => dispatch('reset')}>Reset</button>
     </>
   );
 }

6d2895c8ad5ee2cf9ed71427cda8c39bfd179b12
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-16 10:42:07 -0600

    Add payload

--- 320-React/reducer-practice/src/App.jsx
+++ 320-React/reducer-practice/src/App.jsx
@@ -8,24 +8,39 @@ function App() {
   function reducer(state, action) {
     switch (action.type) {
       case 'inc':
-        return ++state;
+        return state + action.payload;
       case 'dec':
-        return state - 1;
+        return state - action.payload;
       case 'reset':
         return 0;
+      case 'setNumber':
+        return action.payload;
       default:
         return state;
     }
   }
   //dispatch -> gets info from dispatch and returns new state
   const [count, dispatch] = useReducer(reducer, 0);
+  const [num, setNum] = useState(1);
 
   return (
     <>
       <div>Counter: {count}</div>
-      <button onClick={() => dispatch({ type: 'inc' })}>Increment</button>
-      <button onClick={() => dispatch({ type: 'dec' })}>Decrement</button>
+      <button onClick={() => dispatch({ type: 'inc', payload: num })}>
+        Increment
+      </button>
+      <button onClick={() => dispatch({ type: 'dec', payload: num })}>
+        Decrement
+      </button>
+      <button onClick={() => dispatch({ type: 'setNumber', payload: num })}>
+        Set Number
+      </button>
       <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+      <input
+        type="number"
+        value={num}
+        onChange={(e) => setNum(Number(e.target.value))}
+      />
     </>
   );
 }

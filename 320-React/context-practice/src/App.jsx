11fb224bf2c57958a850a0909939bdc800582e91
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-17 11:28:26 -0600

    Add example

---
+++ 320-React/context-practice/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState, createContext } from 'react';
+import './App.css';
+import Parent from './components/Parent';
+
+//step 1. create a context
+const FamilyContext = createContext();
+
+function App() {
+  const [style, setStyle] = useState({
+    color: 'white',
+    backgroundColor: 'blue',
+  });
+
+  function changeStyle() {
+    let newStyle = {
+      color: style.color === 'white' ? 'black' : 'white',
+      backgroundColor: style.backgroundColor === 'blue' ? 'red' : 'blue',
+    };
+    setStyle(newStyle);
+  }
+
+  return (
+    <div>
+      <button onClick={changeStyle}>Change</button>
+      <br />
+      <br />
+      <span style={style}>GrandParent (App)</span>
+      <br />
+      <br />
+      {/* Step 2: Provide the context {and it's values} to nested components */}
+      <FamilyContext.Provider value={style}>
+        <Parent style={style} changeStyle={changeStyle} />
+      </FamilyContext.Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ 320-React/context-practice/src/components/GreatGrandChild.jsx
@@ -0,0 +1,9 @@
+function GreatGrandChild(props) {
+  return (
+    <div>
+      <span style={props.style}>GreatGrandChild</span> <br />
+    </div>
+  );
+}
+
+export default GreatGrandChild;
